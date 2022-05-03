f3bb455fb18d336a9f2b3950b67f64ffc53c1e22
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-05-03 15:51:56 +0545

    chore:validation

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
     readonly username: string;
+    @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
+    @IsNotEmpty()
     readonly password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
 import { UserResponseInterface } from "./interface/user.interface";
 import { UserService } from "./user.service";
@@ -6,6 +6,7 @@ import { UserService } from "./user.service";
 @Controller()
 export class UserController {
     constructor(private readonly userService: UserService) { }
+    @UsePipes(new ValidationPipe())
     @Post('users')
     async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
         const user = await this.userService.createUser(createUserDto)
