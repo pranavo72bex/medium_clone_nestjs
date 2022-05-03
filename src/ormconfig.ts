bc418a9028c97df75b578a69aa101456d32bac45
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-05-03 10:50:07 +0545

    chore:minor changes

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,9 +4,9 @@ import { DataSourceOptions } from 'typeorm';
 const config: DataSourceOptions = {
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
-    username: 'mediumclone',
-    password: '123',
+    port: parseInt(process.env.port),
+    username: process.env.username,
+    password: process.env.password,
     database: 'mediumclone',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: false,
