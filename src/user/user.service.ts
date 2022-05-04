e85407fbed824b0232ec0845deffd5516112fc97
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-05-04 09:32:10 +0545

    feat:validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/createUser.dto";
@@ -10,7 +10,20 @@ export class UserService {
     constructor(@InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
     ) { }
-    async createUser(createUserDto: CreateUserDto) {
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+        const userByEmail = await this.userRepository.findOne({
+            where: {
+                email: createUserDto.email,
+            }
+        });
+        const userByUser = await this.userRepository.findOne({
+            where: {
+                username: createUserDto.username,
+            }
+        })
+        if (userByEmail || userByUser) {
+            throw new HttpException('Email or username are taken', HttpStatus.UNPROCESSABLE_ENTITY)
+        }
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
         return await this.userRepository.save(newUser);
